f421cf74620257a089e89450fb53a6956d17d98f
unknown <d2020.omkar.chugh@ves.ac.in>
2022-10-29 11:54:58 +0530

    Commit

---
+++ src/resumeData.js
@@ -0,0 +1,60 @@
+let resumeData = {
+    "imagebaseurl":"https://pbs.twimg.com/profile_images/1584025027640725505/9g6e9B7O_400x400.jpg",
+    "name": "Omkar Chugh",
+    "role": "Frontend Developer",
+    "linkedinId":"https://www.linkedin.com/in/omkar-chugh-9711391a4/",
+    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/omkar-chugh-9711391a4/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/riskyg0d",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"http://twitter.com/ChughOmkar",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"I am currently a third year student at The VESIT and pursuing my B.Tech from here. I am a self taught Full Stack Web Developer, currently diving deeper into Machine Learning. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+    "address":"India",
+    "website":"https://github.com/riskyg0d",
+    "education":[
+      {
+        "UniversityName":"VESIT",
+        "specialization":"INFT",
+        "MonthOfPassing":"Aug",
+        "YearOfPassing":"2020",
+        "Achievements":"Some Achievements"
+      },
+
+    ],
+    "work":[
+      {
+        "CompanyName":"Hertzsoft Technologies",
+        "specialization":"PHP Developer",
+        "MonthOfLeaving":"July",
+        "YearOfLeaving":"2021",
+      },
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"Java"
+      },
+      {
+        "skillname":"Python"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+  
+  }
+  
+  export default resumeData
